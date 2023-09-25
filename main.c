38d7b62bd54ade1d21df58445160f312dff00155
KimWonjong123 <bonk159@gmail.com>
2023-09-25 13:29:52 +0000

    file read

--- main.c
+++ main.c
@@ -5,6 +5,25 @@
 #include <sys/stat.h>
 #include <errno.h>
 
-int main(int argc, char **argv) {
-    
+#define FILE_READ_ERROR "File Read Error!\n"
+
+int stringlen(char *string) {
+    int length = 1;
+    while(*string++ != '\0') {
+        length++;
+    }
+    return length;
+}
+
+int main(int argc, char *argv[]) {
+    char *fileName = argv[1];
+    int fd;
+    if((fd = open(fileName, O_RDONLY)) < 0) {
+        write(2, FILE_READ_ERROR, stringlen(FILE_READ_ERROR));
+        exit(-1);
+    }
+
+    close(fd);
+
+    return 0;
 }
